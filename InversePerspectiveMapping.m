dc91f8ae592c4a0fb762b37446ac757f0edffa40
Wen Wen <30362457+nomadseed@users.noreply.github.com>
2018-07-04 14:28:00 -0400

    Add files via upload
    update the IPM both with and without interpolation
    for interpolation itself, a strategy of simply copying the corresponding value is used, which can be substitute by other methods if needed

--- InversePerspectiveMapping.m
+++ InversePerspectiveMapping.m
@@ -19,12 +19,15 @@ beta=0; % yaw angle beta, look left if beta>0
 gamma=0; % roll angle gamma, clockwise if gamma>0
 dx=0; % distance from camera to x,move right if dx>0
 dy=0; % distance from camera to y,move downward if dy>0
-dz=0; % distance from camera to z,step away from frame if dz>0 
+dz=500; % distance from camera to z,step away from frame if dz>0 
 
 w=640; % img width in pixel
 h=480; % img height in pixel
 
-focal=1; % focal length
+focal=0.00351; % focal length in meters, if dz=focal-1, 
+% image has 100% solution for image plane is also at z=1
+s=5.7344e-06; % sx and sy in intrinsic matrix (in meters), 
+% assume all units on the sensor are square, which gives sx=sy=s
 
 %% calculate matrices
 % projection mat from 2D to 3D
@@ -66,8 +69,8 @@ T=[ 1       0       0       dx;
     0       0       0       1];
 
 % Intrinsic mat
-I=[ focal   0       w/2     0;
-    0       focal   h/2     0;
+I=[ focal/s 0       w/2     0;
+    0       focal/s h/2     0;
     0       0       1       0];
 
 % complete transformation
@@ -76,14 +79,12 @@ M=I*(R*T*A);
 %     0.1058      -2.2435     562.2521;
 %     2.1540e-04  -4.3104e-03 1.0];
 
-%% calculate IPM of an image
+%% calculate IPM of an image (without interpolation)
 img=imread('4.jpg'); % original image
 img_ipm=uint8(zeros(h,w,3));% image of IPM
-img_itp=uint8(zeros(h,w,3));% image of interpolation
 for y=1:h
    for x=1:w
-        % transform [t*x,t*y,t]' into [x,y,1]'
-        pos_new=M*[x;y;1];
+        pos_new=M*[x;y;1]; % map the [x,y,1] to [t*x',t*y',t]
         pos_new=pos_new./pos_new(3); % get homogeneous coordinates
         pos_new=uint16(floor(pos_new));
         % draw the pixels located in the new img only
@@ -93,16 +94,40 @@ for y=1:h
    end
 end
 
-%% interpolation after transformation
+%% calculate IPM of an image (with interpolation)
+img_itp=uint8(zeros(h,w,3));% image of interpolation
+Minv=inv(M); % note that Minv*[x;y;1] is no better than M\[x;y;1] in matlab
+for y=1:h
+    for x=1:w
+        pos_bp=M\[x;y;1]; % map the [x',y',1] to [t*x,t*y,t]
+        pos_bp=pos_bp./pos_bp(3); % get homogeneous coordinates
+        if pos_bp(1)>=2 && pos_bp(1)<=w-1 && pos_bp(2)>=2 && pos_bp(2)<=h-1
+            % METHOD 1: copy the value of corresponding integer pixel
+            img_itp(y,x,:)=img(round(pos_bp(2)),round(pos_bp(1)),:);
+            
+            % METHOD 2: take a weighted average of 2-neighbor
+            % calculate L1 distance between float pixel coordinate and
+            % actual pixel, note that Lx, Ly in [0,1)
+%             Lx=pos_bp(1)-floor(pos_bp(1));
+%             Ly=pos_bp(1)-floor(pos_bp(1));
+%             img_itp(y,x,:)=(Lx*img(ceil(pos_bp(2)),ceil(pos_bp(1)),:)+(1-Lx)*img(ceil(pos_bp(2)),floor(pos_bp(1)),:)+Ly*img(ceil(pos_bp(2)),ceil(pos_bp(1)),:)+(1-Ly)*img(floor(pos_bp(2)),ceil(pos_bp(1)),:))/2;
+        end   
+    end
+end
 
 
 %% show the img
 figure(1);
-subplot(1,2,1);
+subplot(2,2,1);
 imshow(img);
-subplot(1,2,2);
+title('Original image');
+
+subplot(2,2,2);
 imshow(img_ipm);
-% subplot(2,2,3);
-% imshow(img_itp);
+title('IPM without interpolation');
+
+subplot(2,2,3);
+imshow(img_itp);
+title('IPM with interpolation');
 
 % End of File %
