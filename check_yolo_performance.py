fc51ccafb238b70f89212633130417a6baeb4113
Wen Wen <30362457+nomadseed@users.noreply.github.com>
2018-07-13 16:46:29 -0400

    Add files via upload
    debug: missing fp

--- check_yolo_performance.py
+++ check_yolo_performance.py
@@ -49,9 +49,10 @@ def getIoU(bbx_benchmark,bbx_detect):
     print('IoU=',iou,'\n')
     '''
     
+    
     return iou
 
-def checkSingleImage(annos_benchmark,annos_detect,totalperformance,IOUthresh):
+def checkSingleImage(imgname,annos_benchmark,annos_detect,totalperformance,IOUthresh):
     # check every detected bbx, add the result to currentperformance
     performance=totalperformance
     # special case 1: no detected, but benchmark has cars, all false negative
@@ -78,52 +79,59 @@ def checkSingleImage(annos_benchmark,annos_detect,totalperformance,IOUthresh):
         
     # common case: both benchmark and detected file has bbx, calculate IoU
     else:
-        detect_poplist=annos_detect.copy()
-        for bbx_benchmark in annos_benchmark:
-            # calculate IoU bbx in detect and bbx in benchmark
-            cnt=0
-            totaldetect=len(annos_detect)
-            for i in range(len(annos_detect)):
-                iou=getIoU(bbx_benchmark,annos_detect[i])
-                
+        benchlist=[] # to store the matched bbx
+        detectlist=[] # to store the matched bbx
+        for i in range(len(annos_benchmark)):
+            # calculate IoU bbx in detect and bbx in benchmark            
+            for j in range(len(annos_detect)):
+                iou=getIoU(annos_benchmark[i],annos_detect[j])
                 # true positive
                 if iou>=IOUthresh:
-                    if bbx_benchmark['category'].lower()==annos_detect[i]['category'].lower():
+                    if annos_benchmark[i]['category'].lower()==annos_detect[j]['category'].lower():
                         performance['leading']['tp']+=1
                         performance['overall']['tp']+=1
                     else:
-                        if annos_detect[i]['category'].lower()=='leading': 
-                            # fp for leading car, tp for all cars
+                        # tp for overall
+                        performance['overall']['tp']+=1
+                        if annos_detect[j]['category'].lower()=='leading': 
+                            # fp for leading car
                             performance['leading']['fp']+=1
-                            performance['overall']['tp']+=1
-                        elif annos_detect[i]['category'].lower()=='sideways':
-                            # fn for leading car, tp for all cars
+                        elif annos_detect[j]['category'].lower()=='sideways':
+                            # fn for leading car
                             performance['leading']['fn']+=1
-                            performance['overall']['tp']+=1       
-                    detect_poplist.pop(i)
-                    break # go to next benchmark if already have one match
+                    
+                    # mark the matched bbx in benchmark
+                    benchlist.append(i)  
+                    detectlist.append(j)
+                    # go to next benchmark if already have one match
+        '''
+        print('imgname',imgname)
+        print('bench',benchlist,'bench len',len(annos_benchmark))
+        print('detect',detectlist,'detect len',len(annos_detect),'\n')
+        '''
+        
+        # find the unmatched in benchmark, fn
+        for i in range(len(annos_benchmark)):
+            # if matched, skip
+            if i in benchlist:
+                continue
+            else:
+                performance['overall']['fn']+=1
+                if annos_benchmark[i]['category']=='leading':
+                    # miss a leading car in detection
+                    performance['leading']['fn']+=1
+        
+        # find the unmatched in detect, fp
+        for i in range(len(annos_detect)):
+            # if matched, skip
+            if i in detectlist:
+                continue
+            else:
+                performance['overall']['fp']+=1
+                if annos_benchmark[i]['category']=='leading':
+                    # miss a leading car in detection
+                    performance['leading']['fp']+=1
                 
-                # searched in anno_detect, but no match
-                if cnt==totaldetect:
-                    # already looked for all anno_detect but no match
-                    if bbx_benchmark['category'].lower()=='leading':
-                        performance['leading']['fn']+=1
-                        performance['overall']['fn']+=1
-                    elif bbx_benchmark['category'].lower()=='sideways':
-                        # tn for leading car
-                        performance['overall']['fn']+=1
-                cnt+=1
-        
-        # all bbx in benchmark are searched, matching detection are poped
-        # if there are some more detecion, they are fp
-        #print(len(detect_poplist))
-        for bbx_detect in detect_poplist:
-            if bbx_detect['category'].lower()=='leading':
-                performance['leading']['fp']+=1
-                performance['overall']['fp']+=1
-            elif bbx_detect['category'].lower()=='sideways':
-                performance['overall']['fp']+=1
-        
     
     return performance
 
@@ -132,7 +140,7 @@ if __name__=='__main__':
     parser.add_argument('--file_path', type=str, 
                         default='D:/Private Manager/Personal File/U of Ottawa/Lab works/2018 winter/YOLO3/darkflow-master/test/leading_car/', 
                         help="File path of input data")
-    parser.add_argument('--IoU',type=float,default=0.5,help='percentage of IoU')
+    parser.add_argument('--IoU',type=float,default=0.05,help='percentage of IoU')
     args = parser.parse_args()
     
     filepath=args.file_path
@@ -163,7 +171,7 @@ if __name__=='__main__':
                 annos_benchmark=benchmark[imgname]['annotations']
             
             # calculate performance
-            totalperformance=checkSingleImage(annos_benchmark,annos_detect,totalperformance,IOUthresh)
+            totalperformance=checkSingleImage(imgname,annos_benchmark,annos_detect,totalperformance,IOUthresh)
     
     # calculate precision, recall and missrate
     precision_leading=totalperformance['leading']['tp']/(totalperformance['leading']['tp']+totalperformance['leading']['fp'])
