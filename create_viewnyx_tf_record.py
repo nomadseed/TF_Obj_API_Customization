f3bfa0720b1c9ec8cf85d58c78fae5b02bf25aaf
Wen Wen <30362457+nomadseed@users.noreply.github.com>
2018-08-13 14:33:09 -0400

    Add files via upload
    add choice for divide dataset into shards or only a single tfrecord file

--- create_viewnyx_tf_record.py
+++ create_viewnyx_tf_record.py
@@ -10,11 +10,19 @@ args:
         annotation,also the path to save the tfrecord file
     folder_number: how many folders do you want to process
     json_label: specify the json file with some label in their name
+    shard_number: divide dataset into shards, 10 shards in total as default,
+        no shards if this value <=1
 
 Example usage:
     python create_viewnyx_tf_record.py 
     --file_path ./your/own/path
     --tfrecord_name train.record
+    
+Note:
+    if raise an error like: UnicodeEncodeError: 'utf-8' codec can't encode
+    character '\udcd5' in position 2593: surrogates not allowed
+    check the file_path and the img_name to ensure the program is provided a 
+    solid path to write tfrecord files
     
 
 @author: Wen Wen
@@ -26,11 +34,12 @@ from __future__ import print_function
 import json
 import os
 import argparse
+import contextlib2
 
 import tensorflow as tf
 
 from object_detection.utils import dataset_util
-
+from object_detection.dataset_tools import tf_record_creation_util
 
 def GetClassID(class_label):
     """
@@ -112,7 +121,9 @@ if __name__ == '__main__':
                         help='set how many folders will be processed')
     parser.add_argument('--json_label',type=str, default='full',
                         help='use part of the name to specify the json file to be processed')
-    
+    parser.add_argument('--shard_number',type=int, default=10,
+                        help='divide dataset into shards, 10 shards in total\
+                        as default. no shards if shard_number<=1')
     args = parser.parse_args()
     filepath=args.file_path
     tfrecordname=args.tfrecord_name
@@ -120,13 +131,16 @@ if __name__ == '__main__':
     jsonlabel=args.json_label
     foldernumber=args.folder_number
     foldercount=0
+    shardnumber=args.shard_number
     
+    tf_exp_list=[]
+    # no shards but only one tfrecord file
     for folder in folderdict:
         # skip the files, choose folders only
         if '.' in folder:
             continue 
         
-        # for debug only, set the number of folders to be processed
+        # for debug, set the number of folders to be processed
         if foldercount>=foldernumber:
             break
         else:
@@ -134,30 +148,41 @@ if __name__ == '__main__':
             
         imagepath=os.path.join(filepath,folder)
         filedict=os.listdir(imagepath)
-        
-        # announce the writer for tfrecord file, it will keep writing until closed
-        writer = tf.python_io.TFRecordWriter(os.path.join(imagepath,tfrecordname))
-        
+            
         for jsonname in filedict:
             if 'json' in jsonname and jsonlabel in jsonname:
                 annotations=json.load(open(os.path.join(imagepath,jsonname)))
-                
                 for i in annotations:
                     # specify the image name
-                    #img_name=imagepath.split('\\')[-1]+'_'+annotations[i]['name'] # for viewnyx part1
-                    img_name=annotations[i]['name'] # for viewnyx part 2
+                    img_name=imagepath.split('\\')[-1]+'_'+annotations[i]['name'] # for viewnyx part1
+                    #img_name=annotations[i]['name'] # for viewnyx part 2
                     
                     tf_example=CreateTFExample(imagepath,img_name,annotations[i])
+                    # save all the examples into a list
+                    tf_exp_list.append(tf_example)
                     
-                    writer.write(tf_example.SerializeToString())
-        
-        # indented samely as announcing the tfrecord
+    # write a tfrecord file
+    if shardnumber<=1:
+        # announce the writer for tfrecord file, it will keep writing until closed
+        writer = tf.python_io.TFRecordWriter(os.path.join(filepath,tfrecordname))
+        writer.write(tf_example.SerializeToString())
         writer.close()
-                
+    else:
+        output_filebase=os.path.join(filepath,tfrecordname)
+
+        with contextlib2.ExitStack() as tf_record_close_stack:
+            output_tfrecords = tf_record_creation_util.open_sharded_output_tfrecords(
+                    tf_record_close_stack, output_filebase, shardnumber)
+            for index, tf_example in enumerate(tf_exp_list):
+                output_shard_index = index % shardnumber
+                output_tfrecords[output_shard_index].write(tf_example.SerializeToString())
         
-        print('Successfully created the TFRecords: {}'.format(os.path.join(imagepath,tfrecordname)))
+        
                     
-    
+            
+    print('Successfully created the TFRecords: {}'.format(os.path.join(imagepath,tfrecordname)))
+             
+            
 '''
 if __name__ == '__main__':
     tf.app.run()
