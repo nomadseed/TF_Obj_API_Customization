26db887aa3577730e70547316ad337366cc62087
Wen Wen <30362457+nomadseed@users.noreply.github.com>
2018-08-13 15:16:28 -0400

    Add files via upload
    add parameter foldername_in_filename for distinguish viewnyx dataset part 1 and 2

--- create_viewnyx_tf_record.py
+++ create_viewnyx_tf_record.py
@@ -12,6 +12,8 @@ args:
     json_label: specify the json file with some label in their name
     shard_number: divide dataset into shards, 10 shards in total as default,
         no shards if this value <=1
+    foldername_in_filename: for viewnyx dataset only, use True for dataset
+        part 2, and False for dataset part 1'
 
 Example usage:
     python create_viewnyx_tf_record.py 
@@ -117,13 +119,16 @@ if __name__ == '__main__':
     parser.add_argument('--tfrecord_name', type=str, 
                         default='train.record', 
                         help="select the file path for image folders")
-    parser.add_argument('--folder_number',type=int, default=2,
+    parser.add_argument('--folder_number',type=int, default=100,
                         help='set how many folders will be processed')
     parser.add_argument('--json_label',type=str, default='full',
                         help='use part of the name to specify the json file to be processed')
     parser.add_argument('--shard_number',type=int, default=10,
                         help='divide dataset into shards, 10 shards in total\
                         as default. no shards if shard_number<=1')
+    parser.add_argument('--foldername_in_filename',type=bool,default=True,
+                        help='for viewnyx dataset only, use True for dataset\
+                        part 2, and False for dataset part 1')
     args = parser.parse_args()
     filepath=args.file_path
     tfrecordname=args.tfrecord_name
@@ -132,6 +137,7 @@ if __name__ == '__main__':
     foldernumber=args.folder_number
     foldercount=0
     shardnumber=args.shard_number
+    foldernameinfilename=args.foldername_in_filename
     
     tf_exp_list=[]
     # no shards but only one tfrecord file
@@ -154,8 +160,11 @@ if __name__ == '__main__':
                 annotations=json.load(open(os.path.join(imagepath,jsonname)))
                 for i in annotations:
                     # specify the image name
-                    img_name=imagepath.split('\\')[-1]+'_'+annotations[i]['name'] # for viewnyx part1
-                    #img_name=annotations[i]['name'] # for viewnyx part 2
+                    if foldernameinfilename:
+                        img_name=annotations[i]['name'] # for viewnyx part 2
+                    else:
+                        img_name=imagepath.split('\\')[-1]+'_'+annotations[i]['name'] # for viewnyx part1
+                    
                     
                     tf_example=CreateTFExample(imagepath,img_name,annotations[i])
                     # save all the examples into a list
@@ -165,7 +174,8 @@ if __name__ == '__main__':
     if shardnumber<=1:
         # announce the writer for tfrecord file, it will keep writing until closed
         writer = tf.python_io.TFRecordWriter(os.path.join(filepath,tfrecordname))
-        writer.write(tf_example.SerializeToString())
+        for tf_example in tf_exp_list:
+            writer.write(tf_example.SerializeToString())
         writer.close()
     else:
         output_filebase=os.path.join(filepath,tfrecordname)
@@ -176,11 +186,8 @@ if __name__ == '__main__':
             for index, tf_example in enumerate(tf_exp_list):
                 output_shard_index = index % shardnumber
                 output_tfrecords[output_shard_index].write(tf_example.SerializeToString())
-        
-        
-                    
-            
-    print('Successfully created the TFRecords: {}'.format(os.path.join(imagepath,tfrecordname)))
+     
+    print('Successfully created the TFRecords under path: {}'.format(filepath))
              
             
 '''
